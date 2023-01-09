477fda766340693d7c0d6a477779d48beed391fa
justintsugranes <justintsugranes@proton.me>
2023-01-09 17:42:57 -0500

    add basic nav

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
+import { images } from '../../constants'
+import './Navbar.scss'
+
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <nav>
+      <div>
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul>
+        {['Home', 'About', 'Work', 'Skills', 'Contact'].map((item) => (
+          <li key={`link-${item}`}>
+            <div>
+              <a href={`#${item}`}>{item}</a>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
 }
 
 export default Navbar
