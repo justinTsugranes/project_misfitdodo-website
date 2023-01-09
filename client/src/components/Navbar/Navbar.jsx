bdbabf019f0214ad94a8db7a950157eca32a45c3
justintsugranes <justintsugranes@proton.me>
2023-01-09 17:49:49 -0500

    add navbar css

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -5,13 +5,13 @@ import './Navbar.scss'
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
       </div>
-      <ul>
+      <ul className="app__navbar-links">
         {['Home', 'About', 'Work', 'Skills', 'Contact'].map((item) => (
-          <li key={`link-${item}`}>
+          <li className="app_flex p-text" key={`link-${item}`}>
             <div>
               <a href={`#${item}`}>{item}</a>
             </div>
