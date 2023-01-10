96f4d793f1b36b0c3f3660cdc4929cf7d4b77587
justintsugranes <justintsugranes@proton.me>
2023-01-10 13:32:38 -0500

    add comments to skills

--- client/src/container/Skills/Skills.jsx
+++ client/src/container/Skills/Skills.jsx
@@ -7,11 +7,14 @@ import { urlFor, client } from '../../client'
 import './Skills.scss'
 
 const Skills = () => {
+  // State to hold the experiences and skills data that we fetch from the client
   const [experiences, setExperiences] = useState([])
   const [skills, setSkills] = useState([])
+  // State to hold the active experience/work so that we can show the tooltip when the mouse is over it
   const [active, setActive] = useState(null)
 
   useEffect(() => {
+    // Fetch the data from the client
     const query = '*[_type == "experiences"]'
     const skillsQuery = '*[_type == "skills"]'
 
@@ -60,6 +63,7 @@ const Skills = () => {
                       whileInView={{ opacity: [0, 1] }}
                       transition={{ duration: 0.5 }}
                       className="app__skills-exp-work"
+                      // onMouseEnter and onMouseLeave handlers to set the active state
                       onMouseEnter={() => setActive(work.name)}
                       onMouseLeave={() => setActive(null)}
                       key={work.name}
@@ -68,6 +72,8 @@ const Skills = () => {
                       <p className="p-text">{work.company}</p>
                     </motion.div>
                     {active === work.name && (
+                      // If the 'active' state variable matches the name of the current work,
+                      // render the Tooltip component and pass in the required props.
                       <Tooltip
                         id={work.name}
                         effect="solid"
@@ -89,7 +95,10 @@ const Skills = () => {
 }
 
 export default AppWrap(
-  MotionWrap(Skills, 'app__skills'),
+  // AppWrap is a higher order component that wraps the Skills component
+  // The first argument is the component to wrap and the second argument is the name of the page.
+  // The third argument is the className that is applied to the main container of the wrapped component.
+  MotionWrap(Skills, 'app__skills'), // MotionWrap is also a higher order component that adds animation to the wrapped component.
   'skills',
   'app__whitebg',
 )
