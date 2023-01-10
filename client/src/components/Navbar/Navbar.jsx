a515af71e75377ea324967316e41f3eccef03518
justintsugranes <justintsugranes@proton.me>
2023-01-09 21:08:03 -0500

    add navbar and header

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { HiMenuAlt4, HiX } from 'react-icons/hi'
+import { motion } from 'framer-motion'
 
 import { images } from '../../constants'
 import './Navbar.scss'
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false)
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
       </div>
       <ul className="app__navbar-links">
-        {['Home', 'About', 'Work', 'Skills', 'Contact'].map((item) => (
-          <li className="app_flex p-text" key={`link-${item}`}>
-            <div>
-              <a href={`#${item}`}>{item}</a>
-            </div>
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
+
+      <div className="app__navbar-menu">
+        {/* Render the menu icon and bind the click event to the `setToggle` function */}
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {/* If the `toggle` state variable is `true`, render a menu */}
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            {/* Render the close icon and bind the click event to the `setToggle` function */}
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {/* Render a list of links */}
+              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                <li key={item}>
+                  {/* Each link has an href that corresponds to an element with an ID matching the link text */}
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
   )
 }
--- client/src/container/Header/Header.jsx
+++ client/src/container/Header/Header.jsx
@@ -1,9 +1,83 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
+import { images } from '../../constants'
 import './Header.scss'
 
+// Define motion.div variants for scaling
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
+}
+
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <div className="app__header app__flex">
+      {/* Animate the header info element */}
+      <motion.div
+        whileInView={{ x: [-11, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          {/* Render the badge */}
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">we are</p>
+              <h1 className="head-text">misfitDodo</h1>
+            </div>
+          </div>
+
+          {/* Render the tag */}
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Digital Media Creators</p>
+            <p className="p-text">Brand Developers</p>
+          </div>
+        </div>
+      </motion.div>
+
+      {/* Animate the header image element */}
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        {/* Render the image */}
+        <img src={images.profile} alt="profile.bg" />
+        {/* Animate the image overlay */}
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 0.5, ease: 'easeInOut' }}
+          src={images.circle}
+          alt="profile_circle"
+          className="overlay_circle"
+        />
+      </motion.div>
+
+      {/* Animate the header circles element */}
+      <motion.div
+        variants={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {/* Render a list of circles */}
+        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+          // The `key` prop is used to uniquely identify each list item
+          <div className="cicle-cmp app__flex" key={`circle-${index}`}>
+            {/* Render each circle image */}
+            <img src={circle} alt="circle" />
+          </div>
+        ))}
+      </motion.div>
+    </div>
+  )
 }
 
 export default Header
