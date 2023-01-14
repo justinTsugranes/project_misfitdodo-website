a4aeebfefa3c37ea3540f04a93a7201e8ed75c57
justintsugranes <justintsugranes@proton.me>
2023-01-14 15:27:36 -0500

    add comments to footer

--- client/src/container/Footer/Footer.jsx
+++ client/src/container/Footer/Footer.jsx
@@ -6,20 +6,25 @@ import { client } from '../../client'
 import './Footer.scss'
 
 const Footer = () => {
+  // Hooks to manage the state of the form
   const [formData, setFormData] = useState({ name: '', email: '', message: '' })
   const [isFormSubmitted, setIsFormSubmitted] = useState(false)
   const [loading, setLoading] = useState(false)
 
-  const { username, email, message } = formData
+  // Destructuring the form data for easy access
+  const { name, email, message } = formData
 
+  // Event handler for when the user types in the input fields
   const handleChangeInput = (e) => {
     const { name, value } = e.target
     setFormData({ ...formData, [name]: value })
   }
 
+  // Event handler for when the user submits the form
   const handleSubmit = () => {
     setLoading(true)
 
+    // Construct the contact object
     const contact = {
       _type: 'contact',
       name: formData.username,
@@ -27,9 +32,11 @@ const Footer = () => {
       message: formData.message,
     }
 
+    // Use the client library to send the contact object to the server
     client
       .create(contact)
       .then(() => {
+        // If the contact was sent successfully
         setLoading(false)
         setIsFormSubmitted(true)
       })
@@ -49,23 +56,26 @@ const Footer = () => {
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
-          <a href="tel:+1 (123) 456-7890" className="p-text">
+          <a href="tel:+1 (310) 598-7936" className="p-text">
             +1 (310) 598-7936
           </a>
         </div>
       </div>
+
       {!isFormSubmitted ? (
         <div className="app__footer-form app__flex">
+          {/* Name */}
           <div className="app__flex">
             <input
               className="p-text"
               type="text"
               placeholder="Your Name"
-              name="username"
-              value={username}
+              name="name"
+              value={name}
               onChange={handleChangeInput}
             />
           </div>
+          {/* Email */}
           <div className="app__flex">
             <input
               className="p-text"
@@ -76,6 +86,7 @@ const Footer = () => {
               onChange={handleChangeInput}
             />
           </div>
+          {/* Message */}
           <div>
             <textarea
               className="p-text"
@@ -85,11 +96,13 @@ const Footer = () => {
               onChange={handleChangeInput}
             />
           </div>
+          {/* Submit */}
           <button type="button" className="p-text" onClick={handleSubmit}>
             {!loading ? 'Send Message' : 'Sending...'}
           </button>
         </div>
       ) : (
+        // Form Submitted
         <div>
           <h3 className="head-text">We're going to be great together!</h3>
         </div>
@@ -98,7 +111,17 @@ const Footer = () => {
   )
 }
 
+// AppWrap and MotionWrap are Higher Order Components (HOC)
+// that are used to add additional functionality to the Footer component
+
+// AppWrap takes 3 arguments
+// 1st argument is the component to be wrapped
+// 2nd argument is the type of the component
+// 3rd argument is the classname to be added to the component
 export default AppWrap(
+  // MotionWrap takes 2 arguments
+  // 1st argument is the component to be wrapped
+  // 2nd argument is the classname to be added to the component
   MotionWrap(Footer, 'app__footer'),
   'contact',
   'app__whitebg',
