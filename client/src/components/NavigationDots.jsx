48f60dbe7d1e95a0ee9445032c4054022a4f3833
justintsugranes <87552618+justinTsugranes@users.noreply.github.com>
2023-07-08 15:02:16 -0400

    erase old code

--- client/src/components/NavigationDots.jsx
+++ client/src/components/NavigationDots.jsx
@@ -1,25 +1,3 @@
-// /* eslint-disable jsx-a11y/control-has-associated-label */
-// /* eslint-disable jsx-a11y/anchor-has-content */
-
-// const NavigationDots = ({ active }) => (
-//   <div className="app__navigation">
-//     {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map(
-//       // this array creates 6 dots
-//       (item, index) => (
-//         <a
-//           href={`#${item}`}
-//           key={item + index}
-//           className="app__navigation-dot"
-//           style={active === item ? { backgroundColor: '#313BAC' } : {}}
-//           aria-label={`Go to ${item}`}
-//         />
-//       ),
-//     )}
-//   </div>
-// )
-
-// export default NavigationDots
-
 const NavigationDots = ({ active }) => {
   const renderNavigationDot = (item, index) => {
     const isActive = active === item
